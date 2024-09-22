bdbdd66a858219c87e3233eaa11016d5347ae988
Sime Roncevic <sroncevic19@gmail.com>
2024-09-22 22:51:26 +0200

    Initial commit

---
+++ demo/src/main/java/com/example/demo/SuperiorTaskerBackendSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperiorTaskerBackendSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperiorTaskerBackendSbApplication.class, args);
+	}
+
+}
